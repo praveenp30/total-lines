eeb5a93707b1ec89c4f423ff6833c13000120275
praveenp30 <praveencrony30@gmail.com>
2020-02-16 13:26:12 +0530

    Added events for statusbar

--- extension.js
+++ extension.js
@@ -1,25 +1,41 @@
 const vscode = require('vscode');
 
+const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 0);
+const commandId = 'extension.totalLines';
+
 function activate(context) {
-	let disposable = vscode.commands.registerCommand('extension.totalLines', function () {
-	const editor = vscode.window.activeTextEditor;
-	if (!editor) {
-		vscode.window.showInformationMessage('No open file!');
-		return;
-	}
+  context.subscriptions.push(vscode.commands.registerCommand(commandId, function () {
+	  vscode.window.showInformationMessage(`${getTotalLines()}   *Happy Coding...*!`);
+  }));
+  
+  statusBarItem.command = commandId;
+  context.subscriptions.push(statusBarItem);
+  
+  context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBarItem));
+  context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(updateStatusBarItem));
 
-	const doc = editor.document;
-	const totalLines = (doc.getText().match(/\n/g) || '').length + 1;
-	const statusBar =vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 0);
-
-	statusBar.text = `Total ${totalLines > 1 ? `Lines:` : `Line:` } ${totalLines}`
-	statusBar.tooltip = `Total Lines`
-	statusBar.show();
-	});
-
-	context.subscriptions.push(disposable);
+	// update status bar item once at start
+  updateStatusBarItem();
 }
-exports.activate = activate;
+
+
+function getTotalLines() {
+  const editor = vscode.window.activeTextEditor;
+  let totalLines = 0;
+  if (editor) {
+    const doc = editor.document;
+    totalLines = (doc.getText().match(/\n/g) || '').length + 1;
+  }
+  return `Total ${totalLines > 1 ? `Lines:` : `Line:` } ${totalLines}`;
+}
+
+function updateStatusBarItem() {
+  const editor = vscode.window.activeTextEditor;
+  statusBarItem.text =  getTotalLines();
+	statusBarItem.tooltip = `Total Lines`
+  editor ? statusBarItem.show() :  statusBarItem.hide() ;
+}
+
 
 function deactivate() {}
 
